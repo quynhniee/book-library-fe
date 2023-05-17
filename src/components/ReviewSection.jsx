56b7ddc93f6c3b2c859561bac94b892cdb70b46a
Sona <hongson1234abb@gmail.com>
2023-05-17 08:29:47 +0700

    fix uiuiuiuiu

--- src/components/ReviewSection.jsx
+++ src/components/ReviewSection.jsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Review from "./Review";
 import { useSelector } from "react-redux";
@@ -7,33 +7,36 @@ import { useParams } from "react-router-dom";
 import TextareaValidator from "./TextareaValidator";
 
 const ReviewSection = () => {
-  const { id } = useParams();
-  const userId = useSelector((state) => state.auth.user?.id);
-  const [reviews, setReviews] = useState();
+	const { id } = useParams();
+	const userId = useSelector((state) => state.auth.user?.id);
+	const [reviews, setReviews] = useState();
 
-  useEffect(() => {
-    api.Review.getBookReviews(id).then((response) => {
-      console.log(response);
-      setReviews(response);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		api.Review.getBookReviews(id).then((response) => {
+			console.log(response);
+			setReviews(response);
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  if (!reviews) return <p>Loading...</p>;
+	if (!reviews) return <p>Loading...</p>;
 
-  return (
-    <Paper style={{ padding: "40px 20px", marginTop: 100 }}>
-      <TextareaValidator reviews={reviews} setReviews={setReviews} />
-      {reviews.map((review) => (
-        <Review
-          review={review}
-          userId={userId}
-          reviews={reviews}
-          setReviews={setReviews}
-        />
-      ))}
-    </Paper>
-  );
+	return (
+		<Paper style={{ padding: "40px 20px", marginTop: 100 }}>
+			<TextareaValidator reviews={reviews} setReviews={setReviews} />
+			<Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
+				Reviews
+			</Typography>
+			{reviews.map((review) => (
+				<Review
+					review={review}
+					userId={userId}
+					reviews={reviews}
+					setReviews={setReviews}
+				/>
+			))}
+		</Paper>
+	);
 };
 
 export default ReviewSection;
